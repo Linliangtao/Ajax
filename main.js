fefb6dcceba80d36da833f413696db39905f95ab
linliangtao <1428129601@qq.com>
2018-09-14 22:44:01 +0800

    升级jQuery.ajax,promise规则

--- main.js
+++ main.js
@@ -6,37 +6,48 @@ window.jQuery =function(nodeOrSelector){
 }
 window.$ =window.jQuery
 
-window.jQuery.ajax = function(url,method,body,success,fail){
-    let request =new XMLHttpRequest()
-    request.open(method,url)    //初始化request    
-    request.onreadystatechange = ()=>{
-        if(request.readyState === 4){
-            if(request.status >=200 && request.status<300 ){
-               success.call(undefined,request.responseText)
-               fail.call(undefined,request)
-            }
-  
-        }
+window.Promise =function(fn){
+    return{
+        then:function(){}
     }
-    request.send(body)
 }
-function f1(responseText){}
-function f2(responseText){}
+
+window.jQuery.ajax = function(url,method,body,headers){
+    return new Promise = (function (resolve,reject){
+        let request =new XMLHttpRequest()
+        request.open(method,url)    //初始化request
+
+        for(let key in headers){
+            let value = headers[key]
+            request.setRequestHeader(key,value)
+        }
+
+        request.onreadystatechange = ()=>{
+            if(request.readyState === 4){
+                if(request.status >=200 && request.status<300 ){
+                resolve.call(undefined,request.responseText)             
+                }else if(request.status >=400){
+                    reject.call(undefined,request)
+                }  
+            }
+        }
+        request.send(body)
+    })
+}
+    
 
 button.addEventListener('click',(e)=>{  
-  window.jQuery.ajax({
+  let promise = window.jQuery.ajax({
     url: '/xxx',
     method: 'get',
-    success:(x)=>{
-        f1.call(undefined,x)
-        f2.call(undefined,x)
-    },
-    fail:(x) =>{
-        console.log(x)
-        console.log(x.status)
-        console.log(x.responseText)
+    headers:{
+        'content-type':'application/x-www-form-urlencoded',
+        'frank': '18'
     }
-  })
- 
-
+    
+})
+    promise.then(
+        (text)=>{console.log(text)},
+        (request)=>{console.log(request)}
+    )
 })
